ccb1f8b7d281449dc98c84f9772bb90d48b1469e
Ryouwa <lingheerdao886@gmail.com>
2025-06-23 15:55:02 +0900

    申請完了ページ完成

--- frontend/src/pages/comp.tsx
+++ frontend/src/pages/comp.tsx
@@ -8,10 +8,10 @@ import "../styles/global/text.css"
 const Comp=()=>{
     return (
         <Layout>
-            <div className="h-full w-full flex-col radius-s background-gray p-3 text-center w-half flex-col justify-content align-items ">
-                <img className="w-9 h-9" src="/ok-img.png" alt="申請完了" />
-                <p className="font-x bold">申請しました</p>
-                <p className="font-md">管理者による承認をお待ちください<br />後日メールでお知らせします</p>
+            <div className="flex-col radius-s bg-gray-1 p-3 text-center w-65p center-posi">
+                <img className="w-18" src="/ok-img.png" alt="申請完了" />
+                <p className="font-50 bold m-0">申請しました!</p>
+                <p className="font-md pb-2">管理者による承認をお待ちください。<br />後日メールでお知らせします。</p>
                 <Link to="/" className="radius-md background-blue white p-1 font-l">ホームに戻る</Link>
             </div>
         </Layout>
