539ec5eead9053029c0709085d3e367227c7b831
Ryouwa <lingheerdao886@gmail.com>
2025-06-24 17:27:56 +0900

    検索ページ完了

--- frontend/src/pages/search.tsx
+++ frontend/src/pages/search.tsx
@@ -4,22 +4,10 @@ import Layout from "../components/Layout/Layout";
 const Search=()=>{
     return (
         <Layout>
-            <div>
-                <div className="center-posi text-center font-xl">
-                    <h2>検索</h2>
-                    <h2 className="gray-2 font-2xl">気になるサークルを簡単検索</h2>
-                    <form>
-                        <table >            
-                            <tr>
-                                
-                                <td>
-                                    <input className="w-full radius-md" id="text" />
-                                </td>
-                            </tr>
-                        </table>
-                    </form>
-                </div>
-                <Link to="/" pt-3 m-3>home</Link>
+            <div className="center-posi text-center font-xl">
+                <h2>検索</h2>
+                <h2 className="gray-2 font-2xl">気になるサークルを簡単検索</h2>
+                <input className="w-full radius-md p-1 border" id="text" placeholder="Search for club..."/>
             </div>
         </Layout>
     )
