e6f34cbb18028a93cb1d81f31440f9bb86ccec76
Ryouwa <lingheerdao886@gmail.com>
2025-06-24 18:42:30 +0900

    AI診断ページ一旦完了

--- frontend/src/pages/ai.tsx
+++ frontend/src/pages/ai.tsx
@@ -4,10 +4,13 @@ import Layout from "../components/Layout/Layout";
 const AIsearch=()=>{
     return (
         <Layout>
-            <div className="center-posi text-center font-xl">
+            <div className="center-posi text-center font-xl p-6 border radius-md pt-1 box-shadow">
                 <h2>AIサークル診断へようこそ！</h2>
-                <h2 className="gray-2 font-2xl">気になるサークルを簡単検索</h2>
-                <input className="w-full radius-md p-1 border" id="text" placeholder="Search for club..."/>
+                <img src="https://dummyimage.com/600x400/000/fff" alt="検索欄" />
+                <label htmlFor="" className="flex">
+                    <input className="w-90p radius-none-right p-1 border" id="text" placeholder="Search for club..."/>
+                    <button type="submit" className="w-10p radisu-none-left border-width-none p-1 background-blue white">送信</button>
+                </label>
             </div>
         </Layout>
     )
