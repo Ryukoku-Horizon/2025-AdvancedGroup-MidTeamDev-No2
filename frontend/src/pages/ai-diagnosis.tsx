4f83ede7b8d9f5d2cf4d08270732b0f6a232fac5
Ryouwa <lingheerdao886@gmail.com>
2025-07-01 20:31:39 +0900

    aiPage

--- frontend/src/pages/ai-diagnosis.tsx
+++ frontend/src/pages/ai-diagnosis.tsx
@@ -4,13 +4,25 @@ import Layout from "../components/Layout/Layout";
 const AIDiagnosis=()=>{
     return (
         <Layout>
-            <div className="center-posi text-center font-xl p-6 border radius-md pt-1 box-shadow">
-                <h2>AIサークル診断へようこそ！</h2>
-                <img src="https://dummyimage.com/600x400/000/fff" alt="検索欄" />
-                <label htmlFor="" className="flex">
-                    <input className="w-90p radius-none-right p-1 border" id="text" placeholder="Search for club..."/>
-                    <button type="submit" className="w-10p radisu-none-left border-width-none p-1 background-blue white">送信</button>
-                </label>
+            <div className="relative flex flex-col items-center text-center p-6 border radius-md pt-1 box-shadow wh-75p center-posi">
+                <h2 className="b-9" >AIサークル診断へようこそ！</h2>
+                
+                <div className="wh-75p ml-0 mr-auto text-center">
+                    <div className="l-0 mb-2">
+                        <div className="speechBubble">
+                            こんにちは！
+                        </div>
+                    </div>
+                    <div className="speechBubble-right">
+                        <p>こんにちは。これは例です。</p>
+                    </div>
+                </div>
+                <div className="absolute b-0 mb-2 max-w-600 w-90p">
+                    <label htmlFor="" className="flex w-full">
+                        <input className="w-90p radius-none-right p-1 border b-0" id="text" placeholder="Search for club..."/>
+                        <button type="submit" className="w-10p radisu-none-left border-width-none p-1 background-blue white">送信</button>
+                    </label>
+                </div>
             </div>
         </Layout>
     )
