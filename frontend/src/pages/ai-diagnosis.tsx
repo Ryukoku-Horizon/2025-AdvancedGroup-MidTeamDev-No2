70ab9ad17fea2ce668d46057132137765210b43e
Ryouwa <lingheerdao886@gmail.com>
2025-07-02 09:15:51 +0900

    ai-diagnosisPage

--- frontend/src/pages/ai-diagnosis.tsx
+++ frontend/src/pages/ai-diagnosis.tsx
@@ -5,18 +5,21 @@ const AIDiagnosis=()=>{
     return (
         <Layout>
             <div className="relative flex flex-col items-center text-center p-6 border radius-md pt-1 box-shadow wh-75p center-posi">
-                <h2 className="b-9" >AIサークル診断へようこそ！</h2>
+                <h2 className="b-9 mb-8" >AIサークル診断へようこそ！</h2>
                 
-                <div className="wh-75p ml-0 mr-auto text-center">
-                    <div className="l-0 mb-2">
+                <div className="wh-75-10p ml-0 mr-auto text-left">
+                    <div className="mb-2">
                         <div className="speechBubble">
                             こんにちは！
                         </div>
                     </div>
-                    <div className="speechBubble-right">
+                </div>
+                <div className="wh-75-10p text-right">
+                    <div className="speechBubble-right  mb-2">
                         <p>こんにちは。これは例です。</p>
                     </div>
                 </div>
+
                 <div className="absolute b-0 mb-2 max-w-600 w-90p">
                     <label htmlFor="" className="flex w-full">
                         <input className="w-90p radius-none-right p-1 border b-0" id="text" placeholder="Search for club..."/>
